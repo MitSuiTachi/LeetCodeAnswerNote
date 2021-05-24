620290243d378a30a59b8dd9ac173503597e11d1
wanghaoping <wanghaoping@xiaomi.com>
2021-05-24 15:54:38 +0800

    [Author:wanghaoping][Progress]更新LeetCode104注释
    Signed-off-by: wanghaoping <wanghaoping@xiaomi.com>

--- 104.二叉树的最大深度.java
+++ 104.二叉树的最大深度.java
@@ -23,10 +23,17 @@
 class Solution {
     public int maxDepth(TreeNode root) {
         if (root != null) {
+//            递归进行，不为空节点则返回左右子树的较大值
+//            此处调用Math.max()方法进行判断
             return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+
+//            一开始使用如下三元运算符进行判断会导致Time Limited
 //            return 1 + (maxDepth(root.left) > maxDepth(root.right) ? maxDepth(root.left) : maxDepth(root.right));
+
         }
         return 0;
+
+//        Version 1.0        
 //        if(root == null) {
 //            return 0;
 //        }
