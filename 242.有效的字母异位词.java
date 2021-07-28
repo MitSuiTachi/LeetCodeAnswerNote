8f03286a6c071841628c6a9e932fa8ff71ab79f1
wanghaoping <wanghaoping@xiaomi.com>
2021-07-28 19:36:54 +0800

    [Author:wanghaoping][Progress]LeetCode242代码
    Signed-off-by: wanghaoping <wanghaoping@xiaomi.com>

---
+++ 242.有效的字母异位词.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=java
+ *
+ * [242] 有效的字母异位词
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] table = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            table[t.charAt(i) - 'a']--;
+            if (table[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
