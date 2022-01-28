9a682725bddd6831d1e6219817cc11886df5225b
wanghaoping <wanghaoping@xiaomi.com>
2022-01-28 10:32:45 +0800

    [Author:MitSuiTachi][Progress]
    1.新增LeetCode539题目的解题思路和注释
    
    Signed-off-by: wanghaoping <wanghaoping@xiaomi.com>

--- 539.最小时间差.java
+++ 539.最小时间差.java
@@ -6,6 +6,10 @@
 
 // @lc code=start
 
+/**
+ * 对于一个时间序列来说，时间差最小的两个时间点一定是相邻的（距离0点最近的时间点和距离0点最远的时间点也可以被视作响铃的时间）
+ * 可以将 HH:MM 的24小时制时间转换为距离0点过去了多少分钟，再进行排序，遍历一次排序后的时间序列，并求相邻时间点的时间差，其最小值即为所求
+ */
 class Solution {
     private static final int TOTAL_MINS = 24 * 60;
 
@@ -25,6 +29,7 @@ class Solution {
         min = Math.min(min, from24[0] + TOTAL_MINS - from24[from24.length - 1]);
         return min;
     }
+
     private int getMinFrom24(String s) {
         String[] times = s.split(":");
         int hour = Integer.valueOf(times[0]);
